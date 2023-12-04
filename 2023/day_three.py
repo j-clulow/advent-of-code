7a523e7283fe5c7da5d202ebfe79625560a32337
j-clulow <jacobjclulow@gmail.com>
2023-12-04 13:13:50 +1100

    Starting Part 2 solution - end of time available
    Added the fundamental changes to the functions and classes from part one to accommodate solution in part two. The SchematicPart got a new field to store the location of the associated gear. The process_schematic function now accepts the x,y co-ords of the source symbol and if it was a gear.

--- 2023/day_three.py
+++ 2023/day_three.py
@@ -3,12 +3,14 @@ class SchematicPart:
 	y = -1
 	length = 0
 	value = 0
+	gear_location = ''
 
-	def __init__(self, x, y, len, val):
+	def __init__(self, x, y, len, val, loc):
 		self.x = x
 		self.y = y
 		self.length = len
 		self.value = val
+		self.gear_location = loc
 
 schematic_parts = []
 
@@ -24,7 +26,7 @@ def is_y_boundary(y, schematic):
 def part_already_counted(x,y):
 	return any(part.x == x and part.y == y for part in schematic_parts)
 
-def process_schematic_part(x,y,schematic):
+def process_schematic_part(x,y,schematic,sym_x,sym_y,is_gear):
 	temp_x = x
 	number_string = ''
 	while schematic[y][temp_x].isdigit():
@@ -40,7 +42,10 @@ def process_schematic_part(x,y,schematic):
 		temp_x += 1
 		if is_x_boundary(temp_x, schematic):
 			break
-	schematic_parts.append(SchematicPart(start_x,y,temp_x-start_x,int(number_string)))
+	if is_gear:
+		schematic_parts.append(SchematicPart(start_x,y,temp_x-start_x,int(number_string),str(sym_x) + ',' + str(sym_y)))
+	else:
+		schematic_parts.append(SchematicPart(start_x,y,temp_x-start_x,int(number_string),''))
 	
 
 def adjacent_numbers(x,y,schematic):
@@ -51,12 +56,11 @@ def adjacent_numbers(x,y,schematic):
 			if is_y_boundary(y+y_adjust, schematic):
 				continue
 			if schematic[y+y_adjust][x+x_adjust].isdigit():
-				process_schematic_part(x+x_adjust,y+y_adjust,schematic)
+				process_schematic_part(x+x_adjust,y+y_adjust,schematic,x,y,schematic[y][x] == '*')
 
 def part_one(schematic):
 	x = 0
 	y = 0
-	count = 0
 	for line in schematic:
 		for char in line:
 			if is_symbol(char):
@@ -64,4 +68,21 @@ def part_one(schematic):
 			x += 1
 		y += 1
 		x = 0
+	print('Solution for Day 3, Part 1:' + str(sum(part.value for part in schematic_parts)))
+
+def part_two(schematic):
+	x = 0
+	y = 0
+	for line in schematic:
+		for char in line:
+			if is_symbol(char):
+				adjacent_numbers(x,y,schematic)
+			x += 1
+		y += 1
+		x = 0
+	valued_gears = {part.gear_location for part in schematic_parts }
+	gear_ratio_total = 0
+	for gear_loc in valued_gears:
+		parts_adjacent = [part.value for part in schematic_parts if part.gear_location == gear_loc]
+		
 	print('Solution for Day 3, Part 1:' + str(sum(part.value for part in schematic_parts)))
