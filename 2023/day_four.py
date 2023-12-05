ffe0d6d18ceaf524fb17705a1bdc3f906bce10a6
j-clulow <jacobjclulow@gmail.com>
2023-12-05 12:55:18 +1100

    Day 4 Part 2 Partially complete
    Partially completed part 2 of day 4. Have to pause here and continue later tonight. The logic has been included, just no time to think about the syntax and do testing.

--- 2023/day_four.py
+++ 2023/day_four.py
@@ -10,10 +10,12 @@ def get_card_numbers(card_info):
 		numbers.remove('')
 	return numbers
 
-def calculate_score(winning_numbers, card_numbers):
-	matches = sum(num in card_numbers for num in winning_numbers)
-	if matches > 0:
+def calculate_matches(winning_numbers, card_numbers):
+	return sum(num in card_numbers for num in winning_numbers)
 
+def calculate_score(winning_numbers, card_numbers):
+	matches = calculate_matches(winning_numbers, card_numbers)
+	if matches > 0:
 		return pow(2,matches-1)
 	else:
 		return 0
@@ -31,3 +33,17 @@ def part_one(pile):
 		total_score += card_score
 		count += 1
 	print("Solution for Day 4, Part 1: " + str(int(total_score)))
+
+def part_two(pile):
+	count = 1
+	scratchcards = dict()
+	for card in pile:
+		scratchcards[str(count)] = 1
+		count += 1
+	count = 1
+	for card in pile:
+		winning_numbers = get_winning_numbers(card)
+		card_numbers = get_card_numbers(card)
+		card_matches = calculate_matches(winning_numbers, card_numbers)
+		# for index = count+1 to count+card_matches
+		# add scratchcards[str(count)] to sctratchcards[str(index)]
